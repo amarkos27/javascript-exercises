054562410fa7fdacdc9d11d769e2357ecf410e92
amarkos27 <armarkos27@gmail.com>
2022-10-24 14:49:13 -0400

    Complete exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elements) {
+    debugger;
+    for(const element of elements){
+        if(array.includes(element)){
+            //find element in array
+            for(let i = 0; i < array.length; i++){
+                //remove it using .splice()
+                let found = array[i] === +element || array[i] === element;
+                if(found){
+                    array.splice(i, 1);
+                }
+            }
+        }
+    }
+    return array;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(...args) {
+    if(args.every(num => (typeof num === 'number') && (num >= 0))){
+        let sum = 0;
+        let firstSmaller = args[0] < args[1];
+        let start = firstSmaller ? args[0] : args[1];
+        let end = firstSmaller ? args[1] : args[0];
+        for(let i = start; i < end + 1; i++){
+            sum += i;
+        }
+        return sum;
+    } else return 'ERROR';
 };
 
 // Do not edit below this line
